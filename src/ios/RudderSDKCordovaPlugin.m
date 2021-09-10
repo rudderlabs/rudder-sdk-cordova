c89bc3ea1f274f49c8424f2ed47aaff628e83d6b
Desu Sai Venkat <saivenkatdesu@gmail.com>
2021-09-10 15:28:58 +0530

    Moved All Api calls to a Synchronous exeuctor in iOS to achieve Sequence

--- src/ios/RudderSDKCordovaPlugin.m
+++ src/ios/RudderSDKCordovaPlugin.m
@@ -17,21 +17,21 @@
 - (void)initialize:(CDVInvokedUrlCommand*)command
 {
     [self.commandDelegate runInBackground:^{
-    CDVPluginResult* pluginResult = nil;
-    NSString* writeKey = [command.arguments objectAtIndex:0];
-    RSConfig* config = [Utils getRudderConfig:[command.arguments objectAtIndex:1]];
-    RSOption* option = [Utils getRudderOption:[command.arguments objectAtIndex:2]];
-    [RSClient getInstance:writeKey config:config options:option];
-    if([RSClient sharedInstance] == nil)
-    {
-       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-    else 
-    {
-       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    }
-    
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        CDVPluginResult* pluginResult = nil;
+        NSString* writeKey = [command.arguments objectAtIndex:0];
+        RSConfig* config = [Utils getRudderConfig:[command.arguments objectAtIndex:1]];
+        RSOption* option = [Utils getRudderOption:[command.arguments objectAtIndex:2]];
+        [RSClient getInstance:writeKey config:config options:option];
+        if([RSClient sharedInstance] == nil)
+        {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        }
+        else
+        {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        }
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
 }
 
@@ -42,12 +42,13 @@
         [RSLogger logWarn:@"Dropping the Identify call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         NSString* userId = [command.arguments objectAtIndex:0];
         NSDictionary* userTraits = [command.arguments objectAtIndex:1];
         RSOption* option = [Utils getRudderOption:[command.arguments objectAtIndex:2]];
         [[RSClient sharedInstance] identify:userId traits:userTraits options:option];
-    }];
+    });
 }
 
 - (void)group:(CDVInvokedUrlCommand*)command
@@ -57,12 +58,13 @@
         [RSLogger logWarn:@"Dropping the Group call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         NSString* groupId = [command.arguments objectAtIndex:0];
         NSDictionary* groupTraits = [command.arguments objectAtIndex:1];
         RSOption* option = [Utils getRudderOption:[command.arguments objectAtIndex:2]];
         [[RSClient sharedInstance]group:groupId traits:groupTraits options:option];
-    }];
+    });
 }
 
 - (void)track:(CDVInvokedUrlCommand*)command
@@ -72,12 +74,13 @@
         [RSLogger logWarn:@"Dropping the Track call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         NSString* eventName = [command.arguments objectAtIndex:0];
         NSDictionary* eventProperties = [command.arguments objectAtIndex:1];
         RSOption* option = [Utils getRudderOption:[command.arguments objectAtIndex:2]];
         [[RSClient sharedInstance]track:eventName properties:eventProperties options:option];
-    }];
+    });
 }
 
 - (void)screen:(CDVInvokedUrlCommand*)command
@@ -87,12 +90,13 @@
         [RSLogger logWarn:@"Dropping the Screen call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         NSString* screenName = [command.arguments objectAtIndex:0];
         NSDictionary* screenProperties = [command.arguments objectAtIndex:1];
         RSOption* option = [Utils getRudderOption:[command.arguments objectAtIndex:2]];
         [[RSClient sharedInstance]screen:screenName properties:screenProperties options:option];
-    }];
+    });
 }
 
 - (void)alias:(CDVInvokedUrlCommand*)command
@@ -102,11 +106,12 @@
         [RSLogger logWarn:@"Dropping the Alias call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         NSString* newId = [command.arguments objectAtIndex:0];
         RSOption* option = [Utils getRudderOption:[command.arguments objectAtIndex:1]];
         [[RSClient sharedInstance] alias:newId options:option];
-    }];
+    });
 }
 
 - (void)reset:(CDVInvokedUrlCommand*)command
@@ -116,9 +121,10 @@
         [RSLogger logWarn:@"Dropping the Reset call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         [[RSClient sharedInstance] reset];
-    }];
+    });
 }
 
 - (void)flush:(CDVInvokedUrlCommand*)command
@@ -128,9 +134,10 @@
         [RSLogger logWarn:@"Dropping the Flush call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         [[RSClient sharedInstance] flush];
-    }];
+    });
 }
 
 - (void)optOut:(CDVInvokedUrlCommand*)command
@@ -140,10 +147,11 @@
         [RSLogger logWarn:@"Dropping the optOut call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         BOOL optOut = [command.arguments objectAtIndex:0];
         [[RSClient sharedInstance] optOut:optOut];
-    }];
+    });
 }
 
 - (void)putDeviceToken:(CDVInvokedUrlCommand*)command
@@ -153,10 +161,11 @@
         [RSLogger logWarn:@"Dropping the putDeviceToken call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         NSString* deviceToken = [command.arguments objectAtIndex:0];
         [[[RSClient sharedInstance] getContext] putDeviceToken:deviceToken];
-    }];
+    });
 }
 
 - (void)setAdvertisingId:(CDVInvokedUrlCommand*)command
@@ -166,10 +175,11 @@
         [RSLogger logWarn:@"Dropping the setAdvertisingId call as SDK is not initialized yet"];
         return;
     }
-    [self.commandDelegate runInBackground:^{
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
         NSString* advertisingId = [command.arguments objectAtIndex:0];
         [[[RSClient sharedInstance] getContext] putAdvertisementId:advertisingId];
-    }];
+    });
 }
 
 - (void)setAnonymousId:(CDVInvokedUrlCommand*)command
@@ -185,23 +195,24 @@
         [RSLogger logWarn:@"Dropping the getRudderContext call as SDK is not initialized yet"];
         return;
     }
-
-    [self.commandDelegate runInBackground:^{
-
-    CDVPluginResult* pluginResult = nil;
-    NSDictionary* context = [[[RSClient sharedInstance] getContext] dict];
-
-    if(context == nil)
-    {
-       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-    else 
-    {
-       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:context];
-    }
     
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }];
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+                  ^{
+        
+        CDVPluginResult* pluginResult = nil;
+        NSDictionary* context = [[[RSClient sharedInstance] getContext] dict];
+        
+        if(context == nil)
+        {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        }
+        else
+        {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:context];
+        }
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    });
 }
 
 
