020f8296b0eaa95cc7219a01a04a226b2a7410c9
1abhishekpandey <abhishekpandey10196@gmail.com>
2022-03-07 19:51:34 +0530

    Implemented Singular

---
+++ integrations/rudder-integration-singular-cordova/src/android/RudderSingularFactoryCordova.java
@@ -0,0 +1,38 @@
+package com.rudderstack.analytics.cordova.factories;
+
+import com.rudderstack.analytics.cordova.RudderSDKCordovaPlugin;
+import com.rudderstack.android.integrations.singular.SingularIntegrationFactory;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+
+public class RudderSingularFactoryCordova extends CordovaPlugin {
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+
+    @Override
+    public boolean execute(
+            String action,
+            JSONArray args,
+            CallbackContext callbackContext
+    ) {
+        switch (action) {
+            case "setup":
+                setup(callbackContext);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void setup(CallbackContext callbackContext) {
+        RudderSDKCordovaPlugin.addFactory(SingularIntegrationFactory.FACTORY);
+        callbackContext.success();
+    }
+}
